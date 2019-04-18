3501697105e0c08b1fec666ba9b01f2180428d8f
lovewinds <lovewinds.4i@gmail.com>
2019-04-18 16:28:03 +0900

    [External] Package builder refactoring (fix gtest)
    - Support x64 build
    - Fix file encoding to remove warning

--- external/scripts/packages/gtest/windows.py
+++ external/scripts/packages/gtest/windows.py
@@ -28,6 +28,10 @@ class gtestWindowsBuilder(PlatformBuilder):
             self.env.source_path,
             self.config['name']
         ))
+        project_path = PureWindowsPath('{}/{}/build/googlemock/gtest'.format(
+            self.env.source_path,
+            self.config['name']
+        ))
 
         _check = f'{install_path}/{self.config.get("checker")}'
         if os.path.exists(_check):
@@ -35,15 +39,23 @@ class gtestWindowsBuilder(PlatformBuilder):
             return
 
         self.tag_log("Start building ..")
-        self.env.mkdir_p(build_path)
-        print(build_path)
-        os.chdir(build_path)
 
-        os.system('cmake ..')
+        patch_target = PureWindowsPath('{}/{}/googletest/include/gtest/internal/gtest-internal.h'.format(
+            self.env.source_path,
+            self.config['name']
+        ))
+        self.patch_file_encoding(patch_target)
+
+
+        self.env.mkdir_p(build_path)
+        os.chdir(build_path)
+        os.system('cmake .. -G "Visual Studio 14 2015 Win64"')
+
+        os.chdir(project_path)
+        # As CMake generates project file, so it can't be proceed in pre()
         self.patch_gtest("gtest.vcxproj")
         self.patch_gtest("gtest_main.vcxproj")
 
-        # os.system('msbuild g3log.sln /t:g3logger;g3logger_shared /p:Configuration=Release /p:OutDir='+build_path_rel)
         os.system('''
             msbuild gtest.sln \
                 /maxcpucount:{} \
@@ -51,8 +63,12 @@ class gtestWindowsBuilder(PlatformBuilder):
                 /p:PlatformToolSet={} \
                 /p:Configuration=Release \
                 /p:Platform=x64 \
-                /p:OutDir={}'''.format(
+                /p:OutDir={}\\'''.format(
                     self.env.NJOBS, self.env.compiler_version, install_path))
+
+                # /p:NoWarn=4819 \
+                # /p:WarningLevel=0 \
+                # /clp:ErrorsOnly \
 
     def patch_gtest(self, path):
         msvc_ns_prefix = "{http://schemas.microsoft.com/developer/msbuild/2003}"
@@ -61,14 +77,37 @@ class gtestWindowsBuilder(PlatformBuilder):
         root = tree.getroot()
 
         list = root.findall(msvc_ns_prefix+"ItemDefinitionGroup")
+        patched = False
         for child in list:
             item = child.find(msvc_ns_prefix+"ClCompile")
             item = item.find(msvc_ns_prefix+"RuntimeLibrary")
             if item.text == 'MultiThreaded':
                 item.text = "MultiThreadedDLL"
+                patched = True
             elif item.text == 'MultiThreadedDebug':
                 item.text = "MultiThreadedDebugDLL"
+                patched = True
 
-        self.tag_log("Patched")
+        if patched:
+            self.tag_log("Patched")
+        else:
+            self.tag_log("It seems to be patched already")
 
         tree.write(path, encoding="utf-8", xml_declaration=True)
+
+    def patch_file_encoding(self, path):
+        import codecs
+        BLOCKSIZE = 1048576 # or some other, desired size in bytes
+
+        from shutil import copyfile
+        backup = f'{path}.old'
+        if not os.path.exists(backup):
+            copyfile(path, backup)
+
+        with codecs.open(backup, "r", "utf-8") as sourceFile:
+            with codecs.open(path, "w", "utf-16") as targetFile:
+                while True:
+                    contents = sourceFile.read(BLOCKSIZE)
+                    if not contents:
+                        break
+                    targetFile.write(contents)
