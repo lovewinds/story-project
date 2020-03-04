fd9ed8209ea467b509a4b4627a0193330739cee6
lovewinds <lovewinds.4i@gmail.com>
2020-03-04 14:55:36 +0900

    Apply pep8 to setup modules

--- setup.py
+++ setup.py
@@ -3,31 +3,37 @@
 import os
 from distutils.core import setup, Extension
 
-PLATFORM=os.getenv('_PLATFORM')
+
+PLATFORM = os.getenv('_PLATFORM')
 if PLATFORM is None:
-	PLATFORM='Linux'
+    PLATFORM = 'Linux'
 print("[PLATFORM]: {}".format(PLATFORM))
 
-setup(name="story",
-	py_modules = ["story"],
-	ext_modules=[
-		Extension("_story",
-			["scripting/SpLogHelper_wrap.cxx"],
-			include_dirs=[
-				'src/',
-				'external/build/target/{}/include/SDL2/'.format(PLATFORM),
-				'external/build/source/{}/g3log/src/'.format(PLATFORM),
-				'external/build/source/{}/cppzmq/'.format(PLATFORM),
-				'external/build/source/{}/zeromq/include/'.format(PLATFORM),
-				'scripting/'
-			],
-			library_dirs=[
-				'external/build/target/{}/lib'.format(PLATFORM),
-				'build'
-			],
-			libraries=['g3logger', 'zmq', 'story-project'],
-			extra_compile_args=['-std=c++11', '-DSWIG_PYTHON_INTERPRETER_NO_DEBUG'],
-		)
-	],
-	package_dir={'':'scripting'},
+setup(
+    name="story",
+    py_modules=["story"],
+    ext_modules=[
+        Extension(
+            "_story",
+            ["scripting/SpLogHelper_wrap.cxx"],
+            include_dirs=[
+                'src/',
+                'external/build/target/{}/include/SDL2/'.format(PLATFORM),
+                'external/build/source/{}/g3log/src/'.format(PLATFORM),
+                'external/build/source/{}/cppzmq/'.format(PLATFORM),
+                'external/build/source/{}/zeromq/include/'.format(PLATFORM),
+                'scripting/'
+            ],
+            library_dirs=[
+                'external/build/target/{}/lib'.format(PLATFORM),
+                'build'
+            ],
+            libraries=['g3logger', 'zmq', 'story-project'],
+            extra_compile_args=[
+                '-std=c++11',
+                '-DSWIG_PYTHON_INTERPRETER_NO_DEBUG'
+            ],
+        )
+    ],
+    package_dir={'': 'scripting'},
 )
--- setup_win.py
+++ setup_win.py
@@ -2,27 +2,34 @@
 # setup.py
 from distutils.core import setup, Extension
 
-setup(name="story",
-	py_modules = ["story"],
-	ext_modules=[
-		Extension("_story",
-			["scripting/SpLogHelper_wrap.cxx"],
-			include_dirs=[
-				'src/',
-				'external/build/target/Windows/include/SDL2/',
-				'external/build/source/Windows/g3log/src/',
-				'external/build/source/Windows/cppzmq/',
-				'external/build/source/Windows/zeromq/include/',
-				'scripting/'
-			],
-			library_dirs=[
-				'external/build/target/Windows/lib',
-				'external/build/target/Windows/Debug',
-				'build/Debug'
-			],
-			libraries=['dbghelp', 'g3logger', 'libzmq', 'libstory-project'],
-			extra_compile_args=['-std=c++11', '/EHsc', '-DSWIG_PYTHON_INTERPRETER_NO_DEBUG'],
-		)
-	],
-	package_dir={'':'scripting'},
+
+setup(
+    name="story",
+    py_modules=["story"],
+    ext_modules=[
+        Extension(
+            "_story",
+            ["scripting/SpLogHelper_wrap.cxx"],
+            include_dirs=[
+                'src/',
+                'external/build/target/Windows/include/SDL2/',
+                'external/build/source/Windows/g3log/src/',
+                'external/build/source/Windows/cppzmq/',
+                'external/build/source/Windows/zeromq/include/',
+                'scripting/'
+            ],
+            library_dirs=[
+                'external/build/target/Windows/lib',
+                'external/build/target/Windows/Debug',
+                'build/Debug'
+            ],
+            libraries=['dbghelp', 'g3logger', 'libzmq', 'libstory-project'],
+            extra_compile_args=[
+                '-std=c++11',
+                '/EHsc',
+                '-DSWIG_PYTHON_INTERPRETER_NO_DEBUG'
+            ],
+        )
+    ],
+    package_dir={'': 'scripting'},
 )
