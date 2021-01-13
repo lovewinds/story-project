e07db2e7e8acc54aab70a72e2c7e8c5e0b0e9aa4
lovewinds <lovewinds.4i@gmail.com>
2021-01-13 21:34:21 +0900

    Fix build issues

--- test/graphic/MovementTest.cpp
+++ test/graphic/MovementTest.cpp
@@ -4,7 +4,7 @@
 #include "graphic/movement/Movement.hpp"
 
 TEST(MovementTest, SimpleCreation) {
-  story::Graphic::Movement m1(10.0, 20.0, 3.14);
+  story::Graphic::Movement::Movement m1(10.0, 20.0, 3.14);
 
   EXPECT_EQ(m1.getX(), 10.0);
   EXPECT_EQ(m1.getY(), 20.0);
@@ -12,9 +12,9 @@ TEST(MovementTest, SimpleCreation) {
 }
 
 TEST(MovementTest, Add) {
-  story::Graphic::Movement m1(10.0, 20.0, 3.14);
-  story::Graphic::Movement m2(20.0, 40.0, 0.0);
-  story::Graphic::Movement m3 = m1 + m2;
+  story::Graphic::Movement::Movement m1(10.0, 20.0, 3.14);
+  story::Graphic::Movement::Movement m2(20.0, 40.0, 0.0);
+  story::Graphic::Movement::Movement m3 = m1 + m2;
 
   EXPECT_EQ(m3.getX(), 30.0);
   EXPECT_EQ(m3.getY(), 60.0);
